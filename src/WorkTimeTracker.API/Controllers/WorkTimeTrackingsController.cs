edee78ecc6dfe1a774864aa523c2b2efbeef32b0
Muhammad-1026 <bekbutaevmuhammad1026@gmail.com>
2025-07-10 09:38:45 +0500

    fix: исправлены точки

--- src/WorkTimeTracker.API/Controllers/WorkTimeTrackingsController.cs
+++ src/WorkTimeTracker.API/Controllers/WorkTimeTrackingsController.cs
@@ -1,10 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace WorkTimeTracker.API.Controllers;
-public class WorkTimeTrackingsController : Controller
-{
-    public IActionResult Index()
-    {
-        return View();
-    }
-}
+public class WorkTimeTrackingsController : ControllerBase { }
